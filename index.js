f40c8278ec8af545a0ed977e9d5fd9f4780df4dc
nickdebesten <36811561+nickdebesten@users.noreply.github.com>
2023-01-05 16:42:55 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 80
+app.listen(port, () => {
+    console.log(`app running on port ${port}`)
+})
+app.use("/fonts", express.static(__dirname + "/fonts"));
+app.use("/img", express.static(__dirname + "/img"));
+app.use("/js", express.static(__dirname + "/js"));
+app.use("/css", express.static(__dirname + "/css"));
+app.get(["/", "/index.html"], (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+  })
