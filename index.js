de49e2773e13f305008ccb78da44df89ab1ad1db
nickdebesten <nick.heijden2@gmail.com>
2023-01-08 13:40:23 +0100

    .

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-const express = require('express')
-const app = express()
-const port = 80
-app.listen(port, () => {
-    console.log(`app running on port ${port}`)
-})
-app.use("/fonts", express.static(__dirname + "/fonts"));
-app.use("/img", express.static(__dirname + "/img"));
-app.use("/js", express.static(__dirname + "/js"));
-app.use("/css", express.static(__dirname + "/css"));
-app.get(["/", "/index.html"], (req, res) => {
-    res.sendFile(__dirname + "/index.html");
-  })const express = require('express')
 const app = express()
 const port = 80
 app.listen(port, () => {
